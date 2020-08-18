386d1e50492de1cc55fd7a2677aeec35865740d1
Lucas Mantovani <lucashmantovani@hotmail.com>
2020-08-18 10:16:55 -0300

    Adding noise mesurement and permission asking

--- src/pages/NoisemeterScreen/index.tsx
+++ src/pages/NoisemeterScreen/index.tsx
@@ -1,41 +1,171 @@
-import React, { useEffect } from 'react';
-import { Text } from 'react-native';
-// import RNSoundLevel from 'react-native-sound-level';
+import React, {
+  useEffect,
+  useState,
+  useCallback,
+  useMemo,
+  useRef,
+} from 'react';
+import { PERMISSIONS, request, check } from 'react-native-permissions';
+import {
+  Platform,
+  Alert,
+  Linking,
+  AppState,
+  AppStateStatus,
+} from 'react-native';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
+import RNSoundLevel from 'react-native-sound-level';
 
 import {
   Container,
   NoisemeterContainer,
   Noisemeter,
-  NoiseInfoContainer,
   NoisemeterPointer,
+  NoiseInfoContainer,
+  NoiseLevel,
+  NoiseDescription,
 } from './styles';
 
-// interface SoundData {
-//   id: number;
-//   rawValue: number;
-//   value: number;
-// }
+interface SoundData {
+  id: number;
+  rawValue: number;
+  value: number;
+}
 
 const NoisemeterScreen: React.FC = () => {
-  // const [soundData, setSoundData] = useState<SoundData>();
+  const { navigate } = useNavigation();
+
+  const [microphoneAllowed, setMicrophoneAllowed] = useState<Boolean>(false);
+  const [isRecording, setIsRecording] = useState<Boolean>(false);
+  const [soundData, setSoundData] = useState<SoundData>({
+    id: -1,
+    rawValue: 0,
+    value: 0,
+  });
+  const formattedSoundData = useMemo(() => {
+    return `${soundData?.rawValue.toPrecision(2)} DB`;
+  }, [soundData]);
+  const appState = useRef(AppState.currentState);
+
+  const handleMicrophonePermission = useCallback(() => {
+    async function handleMicrophonePermissionAsync() {
+      const isMicrophonePermissionGranted = await check(
+        Platform.select({
+          android: PERMISSIONS.ANDROID.RECORD_AUDIO,
+          ios: PERMISSIONS.IOS.MICROPHONE,
+          default: PERMISSIONS.ANDROID.RECORD_AUDIO,
+        }),
+      );
+
+      if (isMicrophonePermissionGranted === 'granted') {
+        setMicrophoneAllowed(true);
+        return;
+      }
+
+      if (isMicrophonePermissionGranted === 'blocked') {
+        Alert.alert(
+          'Permissão necessária',
+          'Precisamos da permissão para utilizar o microfone',
+          [
+            {
+              text: 'Ir para configurações',
+              style: 'default',
+              onPress: () => Linking.openSettings(),
+            },
+            {
+              text: 'Cancelar',
+              style: 'cancel',
+            },
+          ],
+        );
+
+        return;
+      }
+
+      const askPermissionResult = await request(
+        Platform.select({
+          android: PERMISSIONS.ANDROID.RECORD_AUDIO,
+          ios: PERMISSIONS.IOS.MICROPHONE,
+          default: PERMISSIONS.ANDROID.RECORD_AUDIO,
+        }),
+      );
+
+      if (askPermissionResult === 'granted') {
+        setMicrophoneAllowed(true);
+      } else {
+        navigate('InfoList');
+      }
+    }
+
+    handleMicrophonePermissionAsync();
+  }, [navigate]);
+
+  const handleStartRecording = useCallback(() => {
+    handleMicrophonePermission();
+
+    if (!microphoneAllowed || isRecording) {
+      return;
+    }
+
+    RNSoundLevel.start();
+    setIsRecording(true);
+  }, [handleMicrophonePermission, isRecording, microphoneAllowed]);
+
+  const handleStopRecording = useCallback(() => {
+    if (!isRecording) {
+      return;
+    }
+
+    RNSoundLevel.stop();
+    setIsRecording(false);
+  }, [isRecording]);
+
+  const handleStartStopRecording = useCallback(() => {
+    handleStartRecording();
+
+    return () => handleStopRecording();
+  }, [handleStartRecording, handleStopRecording]);
+
+  const handleAppStateChange = useCallback(
+    (nextAppState: AppStateStatus) => {
+      if (
+        appState.current.match(/inactive|background/) &&
+        nextAppState === 'active'
+      ) {
+        handleStartStopRecording();
+      }
+
+      appState.current = nextAppState;
+    },
+    [handleStartStopRecording],
+  );
+
+  useFocusEffect(handleStartStopRecording);
 
   useEffect(() => {
-    // RNSoundLevel.starat();
-    // RNSoundLevel.onNewFrame = (data: SoundData) => {
-    //   setSoundData(data);
-    // };
-    // return () => RNSoundLevel.stop();
-  }, []);
+    RNSoundLevel.onNewFrame = (data: SoundData) => {
+      setSoundData(data);
+    };
+  });
+
+  useEffect(() => {
+    AppState.addEventListener('change', handleAppStateChange);
+
+    return () => {
+      AppState.removeEventListener('change', handleAppStateChange);
+    };
+  }, [handleAppStateChange]);
 
   return (
     <Container>
       <NoisemeterContainer>
-        <Noisemeter colors={['#3bb273', '#e1bc29', '#e15554']}>
+        <Noisemeter>
           <NoisemeterPointer />
         </Noisemeter>
       </NoisemeterContainer>
       <NoiseInfoContainer>
-        <Text>90dB</Text>
+        <NoiseLevel>{formattedSoundData}</NoiseLevel>
+        <NoiseDescription>Nível prejudicial</NoiseDescription>
       </NoiseInfoContainer>
     </Container>
   );
--- src/pages/NoisemeterScreen/styles.ts
+++ src/pages/NoisemeterScreen/styles.ts
@@ -1,11 +1,12 @@
 import styled from 'styled-components/native';
 import LinearGradient from 'react-native-linear-gradient';
 
-export const Container = styled.SafeAreaView`
+export const Container = styled.View`
   flex: 1;
   flex-direction: row;
   justify-content: center;
   align-items: center;
+  padding: 16px;
 `;
 
 export const NoisemeterContainer = styled.View`
@@ -14,8 +15,11 @@ export const NoisemeterContainer = styled.View`
   align-items: center;
 `;
 
-export const Noisemeter = styled(LinearGradient)`
+export const Noisemeter = styled(LinearGradient).attrs({
+  colors: ['#e15554', '#e1bc29', '#3bb273'],
+})`
   width: 70%;
+  max-width: 200px;
   height: 80%;
   border: 0;
   border-radius: 8px;
@@ -35,4 +39,18 @@ export const NoiseInfoContainer = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
+  min-width: 50px;
+`;
+
+export const NoiseLevel = styled.Text`
+  font-size: 32px;
+  font-weight: bold;
+  margin-bottom: 32px;
+  color: #3bb273;
+`;
+
+export const NoiseDescription = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
 `;
